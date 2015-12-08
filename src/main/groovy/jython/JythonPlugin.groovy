8b5c62a7f285b6d058361efa507ca442f582897e
rzabini <romano.zabini@gmail.com>
2015-12-08 11:59:28 +0100

    refactorings: add license header and javadoc

--- src/main/groovy/jython/JythonPlugin.groovy
+++ src/main/groovy/jython/JythonPlugin.groovy
@@ -1,37 +1,51 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package jython
 
 import org.gradle.api.Project
 import org.gradle.api.Plugin
 import org.gradle.api.tasks.Delete
 
-class JythonPlugin implements Plugin<Project>{
-	void apply(Project project){
-		assert org.gradle.api.JavaVersion.current().java7Compatible, "at least Java 7 is needed"
+/**
+ * Enables the gradle project to execute jython code, either as a command or as a script.
+ */
+class JythonPlugin implements Plugin<Project> {
+	void apply(Project project) {
+		assert org.gradle.api.JavaVersion.current().java7Compatible, 'at least Java 7 is needed'
 		project.with {
 
-			apply plugin:"base"
-			apply plugin: "de.undercouch.download"
+			apply plugin: 'base'
+			apply plugin: 'de.undercouch.download'
 			
 			configurations.create('pythonpath')
-			repositories{
+			repositories {
 				mavenCentral()
 			}
 
-			configurations{jython}
+			configurations { jython }
 
-			dependencies{
+			dependencies {
 				jython 'org.python:jython-standalone:2.7.0'
 			}
 			
-			extensions.create("jython", JythonExtension, project)
+			extensions.create('jython', JythonExtension, project)
 		}
 		
-		project.task('cleanJython', type:Delete){
+		project.task('cleanJython', type: Delete) {
 			delete "${project.buildDir}/jython"
 		}
-		
-
 	}
 }
-
-
--- src/main/groovy/jython/JythonTask.groovy
+++ src/main/groovy/jython/JythonTask.groovy
@@ -1,29 +1,48 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package jython
+
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.JavaExec
+
+/**
+ * Allows the execution of jython commands and scripts.
+ */
 class JythonTask extends JavaExec {
 		
-		JythonTask(){
+		JythonTask() {
 			main 'org.python.util.jython'
 			classpath project.configurations.jython.asPath
 		}
 		
 		@TaskAction
-		void exec(){
-			
-			
-			logger.info "pythonpath:"
+		void exec() {
+			logger.info 'pythonpath:'
 			logger.info project.configurations.pythonpath.asPath
 			
-			systemProperties(["python.path":project.configurations.pythonpath.asPath])
+			systemProperties(['python.path': project.configurations.pythonpath.asPath])
 			super.exec()
 		}
-		
-		void script(script){
-			if(script instanceof File)
-				args script
-			else
-				args "-c", script
-		}
-		
+
+        void script (String script) {
+            args '-c', script
+        }
+
+        void script (File script) {
+            args script
+        }
+
 }
